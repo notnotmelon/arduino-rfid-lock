e045d172cfb30c347feb21f1a12fbc6b5dcbfd8a
Zachary Picco <zacharypicco@gmail.com>
2025-04-19 14:02:09 -0500

    Update rfid_lock.ino

--- rfid_lock.ino
+++ rfid_lock.ino
@@ -1,20 +1,25 @@
-#include <SPI.h>
 #include <MFRC522.h>
+#include <SPI.h>
 
-#define RST_PIN         9
-#define SS_PIN          10
-#define SOLENOID_PIN    3
-#define BUZZER_PIN      7
+#define SECRET_CODE "73 41 130 0"
+
+#define RST_PIN 9
+#define SS_PIN 10
+#define SOLENOID_PIN 3
+#define BUZZER_PIN 7
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
 
 void setup() {
-  Serial.begin(9600);   // Initialize serial communications with the PC
-  while (!Serial);    // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
-  SPI.begin();      // Init SPI bus
-  mfrc522.PCD_Init();   // Init MFRC522
-  delay(4);       // Optional delay. Some board do need more time after init to be ready, see Readme
-  mfrc522.PCD_DumpVersionToSerial();  // Show details of PCD - MFRC522 Card Reader details
+  Serial.begin(9600);  // Initialize serial communications with the PC
+  while (!Serial);     // Do nothing if no serial port is opened (added for
+                       // Arduinos based on ATMEGA32U4)
+  SPI.begin();         // Init SPI bus
+  mfrc522.PCD_Init();  // Init MFRC522
+  delay(4);  // Optional delay. Some board do need more time after init to be
+             // ready, see Readme
+  mfrc522.PCD_DumpVersionToSerial();  // Show details of PCD - MFRC522 Card
+                                      // Reader details
   Serial.println(F("Scan your card"));
   pinMode(SOLENOID_PIN, OUTPUT);
   pinMode(BUZZER_PIN, OUTPUT);
@@ -24,137 +29,156 @@ bool islocked = true;
 void loop() {
   solonoidlock();
 
-  // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
-  if ( ! mfrc522.PICC_IsNewCardPresent()) {
-    mfrc522.PCD_Init();   // Init MFRC522
+  // Reset the loop if no new card present on the sensor/reader. This saves the
+  // entire process when idle.
+  if (!mfrc522.PICC_IsNewCardPresent()) {
+    mfrc522.PCD_Init();  // Init MFRC522
     islocked = true;
     return;
   }
 
   // Select one of the cards
-  if ( ! mfrc522.PICC_ReadCardSerial()) {
+  if (!mfrc522.PICC_ReadCardSerial()) {
     islocked = true;
     return;
   }
   islocked = false;
-  // Dump debug info about the card; PICC_HaltA() is automatically called
-  //mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
-        
-   Serial.print("Card UID:");
-for (byte i = 0; i < mfrc522.uid.size; i++) {
-    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-    Serial.print(mfrc522.uid.uidByte[i], HEX);
-} 
-Serial.println();
+
+  String uid = String(mfrc522.uid.uidByte[0]) + " " + String(mfrc522.uid.uidByte[1]) + " " + String(mfrc522.uid.uidByte[2]) + " " + String(mfrc522.uid.uidByte[3]);
+  Serial.println("Card UID: " + uid);
+
+  if (uid != SECRET_CODE) {
+    islocked = true;
+    errorSound();
+    return;
+  }
 
   digitalWrite(SOLENOID_PIN, HIGH);
   rick();
-  //delay(2000);
 }
 
 // the loop function runs over and over again forever
 void solonoidlock() {
-  if (islocked == true){
-    digitalWrite(SOLENOID_PIN, LOW);  // turn the LED on (HIGH is the voltage level)
-  }
-  else{
-    digitalWrite(SOLENOID_PIN, HIGH);   // turn the LED off by making the voltage LOW
+  if (islocked == true) {
+    digitalWrite(SOLENOID_PIN,
+                 LOW);  // turn the LED on (HIGH is the voltage level)
+  } else {
+    digitalWrite(SOLENOID_PIN,
+                 HIGH);  // turn the LED off by making the voltage LOW
   }
 }
 
-#define NOTE_B0  31
-#define NOTE_C1  33
+void errorSound() {
+    tone(BUZZER_PIN, 653);
+    delay(200);
+    noTone(BUZZER_PIN);
+    delay(20);
+    tone(BUZZER_PIN, 653);
+    delay(200);
+    noTone(BUZZER_PIN);
+    delay(20);
+    tone(BUZZER_PIN, 653);
+    delay(200);
+    noTone(BUZZER_PIN);
+    delay(20);
+}
+
+#define NOTE_B0 31
+#define NOTE_C1 33
 #define NOTE_CS1 35
-#define NOTE_D1  37
+#define NOTE_D1 37
 #define NOTE_DS1 39
-#define NOTE_E1  41
-#define NOTE_F1  44
+#define NOTE_E1 41
+#define NOTE_F1 44
 #define NOTE_FS1 46
-#define NOTE_G1  49
+#define NOTE_G1 49
 #define NOTE_GS1 52
-#define NOTE_A1  55
+#define NOTE_A1 55
 #define NOTE_AS1 58
-#define NOTE_B1  62
-#define NOTE_C2  65
+#define NOTE_B1 62
+#define NOTE_C2 65
 #define NOTE_CS2 69
-#define NOTE_D2  73
+#define NOTE_D2 73
 #define NOTE_DS2 78
-#define NOTE_E2  82
-#define NOTE_F2  87
+#define NOTE_E2 82
+#define NOTE_F2 87
 #define NOTE_FS2 93
-#define NOTE_G2  98
+#define NOTE_G2 98
 #define NOTE_GS2 104
-#define NOTE_A2  110
+#define NOTE_A2 110
 #define NOTE_AS2 117
-#define NOTE_B2  123
-#define NOTE_C3  131
+#define NOTE_B2 123
+#define NOTE_C3 131
 #define NOTE_CS3 139
-#define NOTE_D3  147
+#define NOTE_D3 147
 #define NOTE_DS3 156
-#define NOTE_E3  165
-#define NOTE_F3  175
+#define NOTE_E3 165
+#define NOTE_F3 175
 #define NOTE_FS3 185
-#define NOTE_G3  196
+#define NOTE_G3 196
 #define NOTE_GS3 208
-#define NOTE_A3  220
+#define NOTE_A3 220
 #define NOTE_AS3 233
-#define NOTE_B3  247
-#define NOTE_C4  262
+#define NOTE_B3 247
+#define NOTE_C4 262
 #define NOTE_CS4 277
-#define NOTE_D4  294
+#define NOTE_D4 294
 #define NOTE_DS4 311
-#define NOTE_E4  330
-#define NOTE_F4  349
+#define NOTE_E4 330
+#define NOTE_F4 349
 #define NOTE_FS4 370
-#define NOTE_G4  392
+#define NOTE_G4 392
 #define NOTE_GS4 415
-#define NOTE_A4  440
+#define NOTE_A4 440
 #define NOTE_AS4 466
-#define NOTE_B4  494
-#define NOTE_C5  523
+#define NOTE_B4 494
+#define NOTE_C5 523
 #define NOTE_CS5 554
-#define NOTE_D5  587
+#define NOTE_D5 587
 #define NOTE_DS5 622
-#define NOTE_E5  659
-#define NOTE_F5  698
+#define NOTE_E5 659
+#define NOTE_F5 698
 #define NOTE_FS5 740
-#define NOTE_G5  784
+#define NOTE_G5 784
 #define NOTE_GS5 831
-#define NOTE_A5  880
+#define NOTE_A5 880
 #define NOTE_AS5 932
-#define NOTE_B5  988
-#define NOTE_C6  1047
+#define NOTE_B5 988
+#define NOTE_C6 1047
 #define NOTE_CS6 1109
-#define NOTE_D6  1175
+#define NOTE_D6 1175
 #define NOTE_DS6 1245
-#define NOTE_E6  1319
-#define NOTE_F6  1397
+#define NOTE_E6 1319
+#define NOTE_F6 1397
 #define NOTE_FS6 1480
-#define NOTE_G6  1568
+#define NOTE_G6 1568
 #define NOTE_GS6 1661
-#define NOTE_A6  1760
+#define NOTE_A6 1760
 #define NOTE_AS6 1865
-#define NOTE_B6  1976
-#define NOTE_C7  2093
+#define NOTE_B6 1976
+#define NOTE_C7 2093
 #define NOTE_CS7 2217
-#define NOTE_D7  2349
+#define NOTE_D7 2349
 #define NOTE_DS7 2489
-#define NOTE_E7  2637
-#define NOTE_F7  2794
+#define NOTE_E7 2637
+#define NOTE_F7 2794
 #define NOTE_FS7 2960
-#define NOTE_G7  3136
+#define NOTE_G7 3136
 #define NOTE_GS7 3322
-#define NOTE_A7  3520
+#define NOTE_A7 3520
 #define NOTE_AS7 3729
-#define NOTE_B7  3951
-#define NOTE_C8  4186
+#define NOTE_B7 3951
+#define NOTE_C8 4186
 #define NOTE_CS8 4435
-#define NOTE_D8  4699
+#define NOTE_D8 4699
 #define NOTE_DS8 4978
-#define REST      0
+#define REST 0
 
 // change this to make the song slower or faster
 int tempo = 114;
+
+// change this to whichever pin you want to use
+int buzzer = 11;
 
 // notes of the moledy followed by the duration.
 // a 4 means a quarter note, 8 an eighteenth , 16 sixteenth, so on
@@ -162,15 +186,17 @@ int tempo = 114;
 // so -4 means a dotted quarter note, that is, a quarter plus an eighteenth!!
 int melody[] = {
 
-  // Never Gonna Give You Up - Rick Astley
-  // Score available at https://musescore.com/chlorondria_5/never-gonna-give-you-up_alto-sax
-  // Arranged by Chlorondria
+    // Never Gonna Give You Up - Rick Astley
+    // Score available at
+    // https://musescore.com/chlorondria_5/never-gonna-give-you-up_alto-sax
+    // Arranged by Chlorondria
 
-  NOTE_A4,16, NOTE_A4,16, NOTE_B4,16, NOTE_D5,8, NOTE_D5,16,
+    NOTE_A4, 16, NOTE_A4, 16, NOTE_B4, 16, NOTE_D5, 8, NOTE_D5, 16,
 };
 
-// sizeof gives the number of bytes, each int value is composed of two bytes (16 bits)
-// there are two values per note (pitch and duration), so for each note there are four bytes
+// sizeof gives the number of bytes, each int value is composed of two bytes (16
+// bits) there are two values per note (pitch and duration), so for each note
+// there are four bytes
 int notes = sizeof(melody) / sizeof(melody[0]) / 2;
 
 // this calculates the duration of a whole note in ms
@@ -180,7 +206,6 @@ int divider = 0, noteDuration = 0;
 
 void rick() {
   for (int thisNote = 0; thisNote < notes * 2; thisNote = thisNote + 2) {
-
     // calculates the duration of each note
     divider = melody[thisNote + 1];
     if (divider > 0) {
@@ -189,7 +214,7 @@ void rick() {
     } else if (divider < 0) {
       // dotted notes are represented with negative durations!!
       noteDuration = (wholenote) / abs(divider);
-      noteDuration *= 1.5; // increases the duration in half for dotted notes
+      noteDuration *= 1.5;  // increases the duration in half for dotted notes
     }
 
     // we only play the note for 90% of the duration, leaving 10% as a pause
